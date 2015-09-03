c41fe611c187e6ceaa6b8f379a9bf234b44e7241
Smudge <nathan@ngriffith.com>
2015-09-03 12:24:47 -0400

    Remove 'require_relative' and 'load' since I see less value in those use cases

--- lib/require_callbacks.rb
+++ lib/require_callbacks.rb
@@ -1,25 +1,23 @@
 require 'require_callbacks/version'
 
 module RequireCallbacks
-  %w(load require require_relative).each do |m|
-    define_method("after_#{m}") do |name, &block|
-      @after_load ||= {}
-      @after_load[name] ||= []
-      @after_load[name] << block
-    end
+  def after_require(name, &block)
+    @after_require ||= {}
+    @after_require[name] ||= []
+    @after_require[name] << block
+  end
 
-    define_method("before_#{m}") do |name, &block|
-      @before_load ||= {}
-      @before_load[name] ||= []
-      @before_load[name] << block
-    end
+  def before_require(name, &block)
+    @before_require ||= {}
+    @before_require[name] ||= []
+    @before_require[name] << block
+  end
 
-    define_method(m) do |*args|
-      @after_load ||= {}
-      @before_load ||= {}
-      (@before_load[args.first] || []).each(&:call)
-      super(*args) && !!(@after_load[args.first] || []).each(&:call)
-    end
+  def require(*args)
+    @after_require ||= {}
+    @before_require ||= {}
+    (@before_require[args.first] || []).each(&:call)
+    super && !!(@after_require[args.first] || []).each(&:call)
   end
 end
 
